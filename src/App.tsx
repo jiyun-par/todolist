6c75b83af62e50f9260041f04c6370f428e3136d
박지윤 <jyun1937@naver.com>
2022-06-12 22:34:35 +0900

    add helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import ToDoList from "./ToDoList";
+import { Helmet } from "react-helmet";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -35,6 +36,7 @@ const GlobalStyle = createGlobalStyle`
 function App() {
 	return (
 		<>
+			<Helmet title="To Do List"/>
 			<GlobalStyle />
 			<ToDoList />
 		</>
